324726602993528f214aeaf20cd12c939348fdba
shoumik-123 <shabikyeaminshoumik123@gmail.com>
2025-05-31 15:11:05 +0600

    Update

--- Backend/dist/main.js
+++ Backend/dist/main.js
@@ -13,6 +13,7 @@ async function bootstrap() {
         .build();
     const document = swagger_1.SwaggerModule.createDocument(app, config);
     swagger_1.SwaggerModule.setup('api', app, document);
+    app.enableCors();
     await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
