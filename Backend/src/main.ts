1b7c75ac6c6f6f363ac2709c2d3074066573842b
sajidur <sajidur@asabd.org>
2025-06-02 11:47:07 +0600

    cors

--- Backend/src/main.ts
+++ Backend/src/main.ts
@@ -51,7 +51,7 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document); // Swagger UI served at /api
-
+  app.enableCors();
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
