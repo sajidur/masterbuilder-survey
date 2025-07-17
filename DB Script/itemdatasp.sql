35b828b407c2f922b8ea610995532113d62bb7c8
sajidur <sajidur@asabd.org>
2025-07-17 14:12:30 +0600

    reportsql

---
+++ DB Script/itemdatasp.sql
@@ -0,0 +1,25 @@
+DROP PROCEDURE IF EXISTS ItemsData;
+DELIMITER $$
+
+create PROCEDURE ItemsData()
+BEGIN
+    SELECT 
+        m.serialNumber AS moduleSerial,
+        m.name AS moduleName,
+        app.serialNumber AS appSerial,
+        app.name AS appName,
+        menu.serialNumber AS menuSerial,
+        menu.title AS menuTitle,
+        item.*
+    FROM item
+	left JOIN menu ON menu.id = item.menuId
+	left JOIN app ON app.id = menu.appId
+    left join modules m on m.id=app.moduleId
+    ORDER BY 
+        m.serialNumber,
+        app.serialNumber,
+        menu.serialNumber,
+        item.serialNumber;
+END $$
+
+DELIMITER ;
---
+++ DB Script/reportdata.sql
@@ -0,0 +1,41 @@
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `ReportData`()
+BEGIN
+    SELECT 
+        md.serialNumber AS moduleSerial,
+        md.name AS moduleName,
+        app.serialNumber AS appSerial,
+        app.name AS appName,
+        menu.serialNumber AS menuSerial,
+        menu.title AS menuTitle,
+        item.serialNumber AS itemSerial,
+        item.name AS itemName,
+        si.serialNumber AS subItemSerial,
+        si.name AS subItemName,
+        ssi.name AS subSubItemName,
+        sssi.name as subsubsubItemName,
+        fi.fieldGroupCode as fieldGroupCode,
+        fi.serialNumber as fieldGroupCodeSerialNumber,
+        fi.DisplayType as DisplayType,
+        fi.remarks as remarks,
+        dp.*
+    FROM modules md 
+    left JOIN app ON md.id = app.moduleId
+    left JOIN menu ON app.id = menu.appId
+    left JOIN item ON menu.id = item.menuId
+    left JOIN sub_item si ON item.id = si.itemId
+    left JOIN sub_sub_item ssi ON si.id = ssi.subItemId
+	left JOIN sub_sub_sub_item sssi ON ssi.id = sssi.subSubItemId
+	left JOIN field fi ON fi.itemId = item.id
+	left JOIN datapoint dp ON fi.id = dp.dpGroupCode
+    ORDER BY 
+        md.serialNumber,
+        app.serialNumber,
+        menu.serialNumber,
+        item.serialNumber,
+        si.serialNumber,
+        ssi.serialNumber,
+        sssi.serialNumber,
+        fi.serialNumber;
+END$$
+DELIMITER ;
